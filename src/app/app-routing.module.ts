2ce0925506de10c67808f2ada6b2e96e94ff0c40
Martin Krull <martin.krull8@gmail.com>
2021-02-17 20:40:46 +0200

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
+
+// kommentaar ctrl ä
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"cart", component:CartComponent},
+  {path:"**", redirectTo:""},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
